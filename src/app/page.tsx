33e1affcdf55a026f3695b85cc8059b55f4f5403
nicosaboonchi <nico.saboonchi@gmail.com>
2026-04-15 15:00:37 -0700

    feat: refactor AccountsPage to improve data fetching and display structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,77 @@
-import { Header } from "@/components/Header";
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemTitle,
-} from "@/components/ui/item";
-import { createClient } from "@/lib/supabase/server";
-import { fmtCurrency, getLastActivityDate, timeAgo } from "@/lib/utils";
-import { UpdateAccountDialog } from "@/components/UpdateAccountDialog";
-import { DeleteAccountDialog } from "@/components/DeleteAccountDialog";
+import { AccountForm } from "@/components/AccountForm";
+import { AccountsGroup } from "@/components/AccountsGroup";
 import { NetworthChart } from "@/components/NetworthChart";
+import { createClient } from "@/lib/supabase/server";
+import { fmtCurrency } from "@/lib/utils";
 
-export default async function Home() {
+export default async function AccountsPage() {
   const supabase = await createClient();
 
-  // wrap in a promise.all to fetch in parallel
-  const [
-    { data: accounts, error },
-    { data: net_worth, error: netWorthError },
-    { data: type_totals, error: typeTotalsError },
-    { data: networth_history, error: netWorthHistoryError },
-  ] = await Promise.all([
-    supabase
-      .from("accounts")
-      .select("*")
-      .order("created_at", { ascending: true }),
-    supabase.from("net_worth").select("sum").single(),
-    supabase.from("type_totals").select("*"),
-    supabase.rpc("get_net_worth_series", {
-      end_date: new Date().toISOString().split("T")[0],
-      start_date: new Date("2026-04-08").toISOString().split("T")[0],
-    }),
-  ]);
+  const { data: accounts, error } = await supabase
+    .from("accounts")
+    .select("*")
+    .order("created_at", { ascending: true });
 
-  console.log(networth_history);
+  const { data: networthHistory, error: networthError } = await supabase.rpc(
+    "get_net_worth_series",
+    {
+      end_date: new Date().toISOString().split("T")[0],
+      start_date: new Date("2026-04-09").toISOString().split("T")[0],
+    },
+  );
+
+  const { data: networth_total, error: networthTotalError } = await supabase
+    .from("net_worth")
+    .select("sum")
+    .single();
+
+  const { data: grouptotals, error: groupTotalsError } = await supabase
+    .from("grouptotals")
+    .select("*");
+
+  if (networthTotalError) {
+    throw networthTotalError;
+  }
+
+  if (networthError) {
+    throw networthError;
+  }
+
+  if (groupTotalsError) {
+    throw groupTotalsError;
+  }
+
   if (error) {
     throw error;
   }
 
-  if (netWorthError) {
-    throw netWorthError;
-  }
+  const grouped = Object.groupBy(accounts, (account) => account.type);
 
-  if (typeTotalsError) {
-    throw typeTotalsError;
-  }
-
-  if (netWorthHistoryError) {
-    throw netWorthHistoryError;
-  }
-
-  const accountsByType = Object.groupBy(accounts, (account) => account.type);
+  const accountGroups = Object.entries(grouped).map(([type, accts = []]) => ({
+    type,
+    total: accts.reduce((sum, account) => sum + account.balance, 0),
+    accounts: accts,
+  }));
 
   return (
-    <div className="flex flex-col w-full gap-4 p-4">
-      <Header />
-      <NetworthChart
-        data={networth_history}
-        networth={fmtCurrency(net_worth.sum ?? 0)}
-      />
-      <div>
-        <h2 className="text-lg font-semibold">Totals by Type</h2>
-        {type_totals.map((typeTotal) => (
-          <div key={typeTotal.type}>
-            {typeTotal.type}: {fmtCurrency(typeTotal.total ?? 0)}
-          </div>
+    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-[2fr_1fr] md:p-6">
+      <div className="col-span-1 md:col-span-2 max-h-100">
+        <NetworthChart data={networthHistory} networth={networth_total?.sum} />
+      </div>
+      <div className="flex flex-col gap-4">
+        {accountGroups.map((group) => (
+          <AccountsGroup
+            key={group.type}
+            type={group.type.charAt(0).toUpperCase() + group.type.slice(1)}
+            count={group.accounts.length}
+            total={fmtCurrency(group.total)}
+            accounts={group.accounts}
+          />
         ))}
       </div>
-      {Object.entries(accountsByType).map(([type, accounts]) => (
-        <div key={type} className="flex flex-col gap-4">
-          <h2 className="text-lg font-semibold">
-            {type.charAt(0).toUpperCase() + type.slice(1)} Accounts
-          </h2>
-          {accounts?.map((account) => (
-            <Item key={account.id} variant="muted">
-              <ItemContent>
-                <ItemTitle>{account.name}</ItemTitle>
-                <ItemDescription>
-                  {account.type.charAt(0).toUpperCase() + account.type.slice(1)}
-                </ItemDescription>
-              </ItemContent>
-              <ItemContent className="items-end">
-                <ItemTitle>{fmtCurrency(account.balance)}</ItemTitle>
-                <ItemDescription>
-                  {timeAgo(getLastActivityDate(account))}
-                </ItemDescription>
-              </ItemContent>
-              <ItemActions>
-                <UpdateAccountDialog account={account} />
-                <DeleteAccountDialog account={account} />
-              </ItemActions>
-            </Item>
-          ))}
-        </div>
-      ))}
+      <div>
+        <AccountForm />
+      </div>
     </div>
   );
 }
